f9d8814bd026320660caa061c8a31265f97f9ebc
Chris Heller <ctheller12@gmail.com>
2016-07-12 15:49:20 -0400

    added sessions middleware

--- server/app/index.js
+++ server/app/index.js
@@ -7,9 +7,34 @@ app.use(require('./logging.middleware'));
 
 app.use(require('./request-state.middleware'));
 
-app.use(require('./statics.middleware'));
+var session = require('express-session');
+var User = require('../api/users/user.model');
+
+app.use(session({
+  // this mandatory configuration ensures that session IDs are not predictable
+  secret: 'tongiscool',
+  duration: 30*60*1000,
+  activeDuration: 5*60*1000
+}));
+
+app.use(function (req, res, next) {
+  console.log('session', req.session);
+  next();
+});
+
+app.post('/login', function(req, res, next){
+	User.findOne({where: req.body })
+	.then(function(user){
+		if (!user) return res.sendStatus(401);
+		req.session.userId = user.id;
+		res.sendStatus(204);
+	})
+	.catch(next);
+});
 
 app.use('/api', require('../api/api.router'));
+
+app.use(require('./statics.middleware'));
 
 var validFrontendRoutes = ['/', '/stories', '/users', '/stories/:id', '/users/:id', '/signup', '/login'];
 var indexPath = path.join(__dirname, '..', '..', 'public', 'index.html');
